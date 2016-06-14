5c9d6d26381068d19e20405ab69f723acf895326
FRANCO L <FRANCO L@158.170.63.229>
2016-06-14 17:48:22 -0300

    asdasdsad

--- src/Modelo/Dado.java
+++ src/Modelo/Dado.java
@@ -7,16 +7,18 @@ import Controlador.ControladorBatalla;
 //Declaraacion clase
 public class Dado {
     //Atributos
-    ImageIcon[] caras;
     public int i;
     public int j;
     public int[][] forma1 = {{0,0},{0,1},{0,2},{1,1},{1,2},{1,3}};
     public int[][] forma2 = {{0,0},{0,1},{0,2},{1,1},{1,2},{1,3}};
+    String [] caras;
     //Constructor
     public Dado(){
+        String[] a ={"MAGIA","ATAQUE","INVOCAR","MOVIMIENTO","MOVIMIENTO","TRAMPA"};       
+        this.caras = a;
     }
     //Métodos
-    public ImageIcon resultado(){
+    public String resultado(){
         Random rGenerador = new Random();
         return caras[rGenerador.nextInt(6)];
     }
