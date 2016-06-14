471408ee06682e8b8d78febea4a48368e835e7b3
FRANCO L <FRANCO L@158.170.63.229>
2016-06-14 17:49:34 -0300

    asdasdasd

--- src/Modelo/Tablero.java
+++ src/Modelo/Tablero.java
@@ -6,20 +6,22 @@ import javax.swing.*;
 import java.awt.event.ActionListener;
 import Vista.VistaBatalla;
 //Declaracion clase
-public class Tablero extends javax.swing.JPanel implements ActionListener {
-    Casilla[][] boton;
-    public Tablero(JPanel tablero){
-        /*tablero.setLayout(new java.awt.GridLayout(15,15));
-        boton = new Casilla[15][15];
+public class Tablero{
+    public Casilla[][] board;
+    public Tablero(){
+        board = new Casilla[15][15];
         for (int i = 0 ; i<15 ; i++){
             for (int j = 0 ; j<15 ; j++){
-                boton[i][j] = new Casilla();
-                boton[i][j].addActionListener(boton[i][j]);
-                tablero.add(boton[i][j]);
+                board[i][j] = new Casilla();
             }
-        }*/
+        }
     }
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    public void pintar(int i,int j,int[][] forma){
+        board[forma[0][0]+i][forma[0][1]+j].setBackground(Color.yellow);
+        board[forma[1][0]+i][forma[1][1]+j].setBackground(Color.yellow);
+        board[forma[2][0]+i][forma[2][1]+j].setBackground(Color.yellow);
+        board[forma[3][0]+i][forma[3][1]+j].setBackground(Color.yellow);
+        board[forma[4][0]+i][forma[4][1]+j].setBackground(Color.yellow);
+        board[forma[5][0]+i][forma[5][1]+j].setBackground(Color.yellow);
     }
 }
